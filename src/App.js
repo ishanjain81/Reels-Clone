6bdeef0189f33fdd558b824b7f9e311037fb8903
Ishan Jain <ishanjaincool81@gmail.com>
2021-10-29 17:17:03 +0530

    Setting Observer API for autoplay videos

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Profile from './Components/Profile';
 import {BrowserRouter ,Switch,Route} from 'react-router-dom';
 import {AuthProvider} from './Context/AuthContext';
 import PrivateRoute from './Components/PrivateRoute';
+import Ioa from './Components/Ioa';
 
 function App() {
   return (
--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -36,6 +36,27 @@ function Posts({userData}) {
         })
         return unsub
     },[userData])
+    const callback = (entries) => {
+        entries.forEach((entry)=>{
+            let ele = entry.target.childNodes[0]
+            // console.log(ele)
+            ele.play().then(()=>{
+                if(!ele.paused && !entry.isIntersecting){
+                    ele.pause()
+                }
+            })
+        })
+    }
+    let observer = new IntersectionObserver(callback, {threshold:0.6});
+    useEffect(()=>{
+        const elements = document.querySelectorAll(".videos")
+        elements.forEach((element)=>{
+            observer.observe(element)
+        })
+        return ()=>{
+            observer.disconnect();
+        }
+    },[posts])
     return (
         <div>
             {
